350a4e00453a6803e834035410be24d1830aeabb
Victor-Quqi <v1756251285@gmail.com>
2025-11-11 14:47:35 +0800

    docs: 更新了总纲和开发优先级

--- Connect.cs
+++ Connect.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using Extensibility;
-using Microsoft.Office.Core;
 using Microsoft.Office.Interop.OneNote;
 
 namespace TeXShift
@@ -11,7 +8,7 @@ namespace TeXShift
     [Guid("1EE8F914-ECBD-4709-92C0-E770851C4966")]
     [ProgId("TeXShift.Connect")]
     [ComVisible(true)]
-    public class Connect : IDTExtensibility2, IRibbonExtensibility
+    public class Connect
     {
 
     }
